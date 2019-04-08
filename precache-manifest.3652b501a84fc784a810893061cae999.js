213b8a506e554177ab68e693eda9c0475c56567e
Phumudzo Nevhutala <Phumudzo.Nevhutala@americantower.com>
2019-04-08 14:28:07 +0200

    Updates

---
+++ precache-manifest.3652b501a84fc784a810893061cae999.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b85aee68c5a6b9c7b162f25da73ad2de",
+    "url": "/static/media/resume_example.b85aee68.pdf"
+  },
+  {
+    "revision": "3f451ad3767a1a3c1080",
+    "url": "/static/css/main.4d440a74.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0d84d376ecf4dca44981f8af52a86ea7",
+    "url": "/static/media/phumie-logo-white.0d84d376.png"
+  },
+  {
+    "revision": "7ed50070a88539ca26f3",
+    "url": "/static/js/2.7ed50070.chunk.js"
+  },
+  {
+    "revision": "93613017f3ec7265d726df1631c5e98f",
+    "url": "/static/media/phumie-logo-black.93613017.png"
+  },
+  {
+    "revision": "3f451ad3767a1a3c1080",
+    "url": "/static/js/main.3f451ad3.chunk.js"
+  },
+  {
+    "revision": "c79d23ce1222a265243b9c7ba3891890",
+    "url": "/static/media/phumie-avatar.c79d23ce.png"
+  },
+  {
+    "revision": "14a0f6532fd0eabe80be2640d950f845",
+    "url": "/static/media/angular.14a0f653.png"
+  },
+  {
+    "revision": "7b37c44faa762180bc4c7166371fda63",
+    "url": "/static/media/phumie_drawing_high.7b37c44f.png"
+  },
+  {
+    "revision": "7ed50070a88539ca26f3",
+    "url": "/static/css/2.5764c337.chunk.css"
+  },
+  {
+    "revision": "d25592c022ee213b0d1b7f5446ae2575",
+    "url": "/index.html"
+  }
+];
